99fc436ece213fc405b32934d0e3ce17140f2990
Merlin Cooper <MerlinC@gmx.de>
2023-03-18 10:16:29 +0100

    Changes Settings Scope for iRacingReplayDirector-PlugIn Tester form USER to APPLICATION

--- plugins/iRacingDirector.Plugin.Tester/Properties/Settings.Designer.cs
+++ plugins/iRacingDirector.Plugin.Tester/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace iRacingDirector.Plugin.Tester.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -23,39 +23,30 @@ namespace iRacingDirector.Plugin.Tester.Properties {
             }
         }
         
-        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("")]
         public string PluginAssemblyFileName {
             get {
                 return ((string)(this["PluginAssemblyFileName"]));
             }
-            set {
-                this["PluginAssemblyFileName"] = value;
-            }
         }
         
-        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("")]
         public string BackgroundTestImageFileName {
             get {
                 return ((string)(this["BackgroundTestImageFileName"]));
             }
-            set {
-                this["BackgroundTestImageFileName"] = value;
-            }
         }
         
-        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("")]
         public string SampleSessionDataFileName {
             get {
                 return ((string)(this["SampleSessionDataFileName"]));
-            }
-            set {
-                this["SampleSessionDataFileName"] = value;
             }
         }
     }
